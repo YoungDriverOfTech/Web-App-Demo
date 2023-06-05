f87e89facc6c20cb95b722ca582be71406948f18
liangxiaole <xiaoleliang045@gmail.com>
2023-06-05 23:12:48 +0900

    update

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 2. 为什么浏览器中会显示 index.jsp的内容。因为通过 Tomcat 服务器启动的 web应用 的默认欢迎页是 index.jsp。
 3. web.xml 的文件名能不能改，WEB-INF目录名能不能改？不可以改，这是Servlet规范的一部分。
 4. Main目录下创建的目录能否不叫webapp。可以叫其他名字，这个目录的名字不是 Servlet 规范的一部分。
+
+### 使用jar创建第一个servlet
+1. 下载servlet的jar包，版本确认需要看tomcat的官网，java8以及以上应该有servlet4
+2. Idea 里面找到project structure的Library，把servlet的jar加入
+3. 找到tomcat安装目录把例子的web.xml拷贝出来修改一下。 （/Library/tomcat/webapps/examples/WEB-INF）
+4. 自己的servlet类实现Servlet接口，追加自己的逻辑代码即可
--- src/main/java/com.app.web/MyFirstServlet.java
+++ src/main/java/com.app.web/MyFirstServlet.java
@@ -1,4 +1,39 @@
 package com.app.web;
 
-public class MyFirstServlet {
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyFirstServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        PrintWriter writer = servletResponse.getWriter();
+        writer.print("<html>");
+        writer.print("<body>");
+        writer.print("<h2>");
+        writer.print("my first service by using jar file");
+        writer.print("</h2>");
+        writer.print("</body>");
+        writer.print("</html>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
